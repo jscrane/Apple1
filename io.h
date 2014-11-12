0d20af47d6952c7b191715e29c3d9f232f44671f
Stephen Crane <jscrane@gmail.com>
2014-11-12 08:28:32 +0000

    use smaller font; add shiftmap

--- io.h
+++ io.h
@@ -1,23 +1,25 @@
 #ifndef _IO_H
 #define _IO_H
 
-class io: public UTFTDisplay {
+class io: public UTFTDisplay, public Keyboard {
 public:
 	io(): UTFTDisplay(256) {}
 
 	void operator=(byte);
 	operator byte();
 
-	void reset();
-	void down(unsigned scan);
-	void up(unsigned scan);
+	virtual void reset();
+	virtual void down(byte scan);
+	virtual void up(byte scan);
 
 	void checkpoint(Stream &);
 	void restore(Stream &);
 
 private:
 	void display(byte);
+	void draw(char, int, int);
 
+	bool _shift;
 };
 
 #endif
